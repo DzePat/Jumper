1cc239d11feffd4ca37e7d81cb2610cb02d116e4
DzePat <ddzedas@gmail.com>
2023-05-15 13:41:59 +0200

    added basic white window and a shape circle from the pygames tutorial

--- Jumper.py
+++ Jumper.py
@@ -1,2 +1,31 @@
+import pygame
+
 def Jumper():
-    # to be added
+    from pygame.locals import(
+        K_UP,
+        K_DOWN,
+        K_LEFT,
+        K_RIGHT,
+        K_ESCAPE,
+        KEYDOWN,
+        QUIT
+        )
+
+
+    pygame.init()
+    screen = pygame.display.set_mode([800,800])
+    running = True
+    while(running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        screen.fill([255,255,255])
+
+        pygame.draw.circle(screen,(0,0,255),(400,400),25)
+
+        pygame.display.flip();
+    pygame.quit();
+
+Jumper()
