45b67eeea183feb3654cd0c21b0c51a1901a5113
DzePat <ddzedas@gmail.com>
2023-05-19 10:28:54 +0200

    pull on laptop needs a commit

--- Jumper.py
+++ Jumper.py
@@ -3,6 +3,7 @@ import pygame
 import random
 
 from pygame.locals import (
+        RLEACCEL,
         K_UP,
         K_DOWN,
         K_LEFT,
@@ -26,8 +27,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((25, 25))
-        self.surf.fill((255, 255, 255))
+        self.surf = pygame.image.load("cat.png").convert()
+        self.surf.set_colorkey((255, 255, 255),RLEACCEL)
         self.rect = self.surf.get_rect()
         self.jump = int(5)
     def update(self, pressed_keys):
